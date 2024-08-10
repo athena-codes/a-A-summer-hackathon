c48ee16001d14960fcf2644ad3561247e51c7f80
Gek <gakidogeck@gmail.com>
2024-08-10 15:18:39 -0700

    trying to get this auth get current user to work

--- app/frontend/src/routes/authRoutes.js
+++ app/frontend/src/routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController');
+const { authenticateUser, registerUser, loginUser } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
+router.get('/current', authenticateUser, (req, res) => {
+    res.status(200).json({ message: 'User authenticated', user: req.currentUser });
+});
 module.exports = router;
