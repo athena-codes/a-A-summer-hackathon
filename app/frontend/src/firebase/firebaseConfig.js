cedf48c36b5c599380a7e2ae43e9c0e95673167f
Gek <gakidogeck@gmail.com>
2024-08-10 13:32:43 -0700

    put firebaseConfig private info in env

--- app/frontend/src/firebase/firebaseConfig.js
+++ app/frontend/src/firebase/firebaseConfig.js
@@ -23,19 +23,19 @@
 
 // // Get a reference to the Firestore service
 // export const db = getFirestore(firebaseApp)
-
+require('dotenv').config();
 const { initializeApp } = require('firebase/app');
 const { getAuth } = require('firebase/auth');
 const { getFirestore } = require('firebase/firestore');
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDhfk198F2ZUgT89ON9brPk5x96rHqfjvA',
-  authDomain: 'hackathon-8dd5b.firebaseapp.com',
-  projectId: 'hackathon-8dd5b',
-  storageBucket: 'hackathon-8dd5b.appspot.com',
-  messagingSenderId: '97407855709',
-  appId: '1:97407855709:web:b9972d85d09d144eaff1b4',
-  measurementId: 'G-1VRV2K96RS'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
